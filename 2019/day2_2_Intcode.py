7dd5764c710650468e3cf3b65bd13c995c64e443
John Eriksson <Svartstrom@users.noreply.github.com>
2019-12-02 12:54:29 +0100

    Some house cleaning

--- 2019/day2_2_Intcode.py
+++ 2019/day2_2_Intcode.py
@@ -1,18 +1,15 @@
 
 with open('day2_2_in.txt','r') as f:
-    b = f.readlines()
-    for a in b:
-        #print(a)
-        a = a.split(',')
-        #print(a)
-        for i, c in enumerate(a):
-            a[i] = int(c)
-
-        #print(len(b))
+    prog_string = f.readlines()
+    for prog in prog_string:
+        command = prog.split(',')
+        for i, command in enumerate(command):
+            org[i] = int(command)
+            
+            
 def run_code(bb):
     op = 0
     index = 0
-    print(bb[0],bb[1],bb[2])
     while op != 99:
         op = bb[index]
 
@@ -24,12 +21,8 @@ def run_code(bb):
             bb[bb[index+3]] = bb[bb[index+1]] * bb[bb[index+2]]
             index += 4
         else:
-            print(op, bb[0], index)
             break
     return bb[0]
-
-org = a
-
 
 ans_ = 0
 noun = 0
@@ -38,8 +31,8 @@ while ans_ != 19690720:
     test = org[:]
     test[1] = noun
     test[2] = verb
+    
     ans_ = run_code(test)
-    print("noun:"+str(noun)+" verb: "+str(verb)+" => ans_:" +str(ans_))
     noun += 1
     if noun > 99:
         noun = 0
@@ -49,6 +42,3 @@ while ans_ != 19690720:
 print("noun: " + str(noun))
 print("verb: " + str(verb))
 
-print(org[0])
-print(a)
-print(org)
